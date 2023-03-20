2c3d97a8c0494373a4601871e6eda37c7c2fcb39
Rodrigo Farinango <rodrigofarii@gmail.com>
2023-03-19 22:03:06 -0600

    feat: infinite scroll

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import ListOfGifs from 'components/ListOfGifs';
 import { useGifs } from 'hooks/useGifs';
+import useNearScreen from 'hooks/useNearScreen';
+import debounce from 'just-debounce-it';
 
 export default function SearchResults({params}){
     const {keyword} = params;
     const {loading, gifs, setPage} = useGifs({keyword});
+    const externalRef = useRef(); 
+    const {isNearScreen} = useNearScreen({
+        externalRef: loading ? null: externalRef,
+        once: false
+    });
 
-    const handleNextPage = () =>{
+/*     const handleNextPage = () =>{
         setPage(prevPage => prevPage + 1);
-    }
+    } */
+
+    const handleNextPage = () => console.log('next page');
+
+    const debounceHandleNextPage = useCallback(debounce(
+        () => setPage(prevPage => prevPage + 1)
+    ), [])
+
+    useEffect(function(){
+        if(isNearScreen) debounceHandleNextPage();
+    }, [debounceHandleNextPage, isNearScreen])
 
     return <>
         {loading
-        ? "Loading...": <><h3>{decodeURI(keyword)}</h3><ListOfGifs gifs={gifs}/></>
+        ? "Loading..."
+        : <>
+            <h3>
+                {decodeURI(keyword)}
+            </h3>
+            <ListOfGifs gifs={gifs}/>
+            <div id="visor" ref={externalRef}></div>
+        </>
         }
-        <br/>
-        <button onClick={handleNextPage}>Get next page</button>
     </>
 };
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,6 +1,6 @@
 import {API_KEY, API_URL} from './settings';
 
-export default function getGifs({limit = 25, keyword = 'morty', page = 0}={}){
+export default function getGifs({limit = 5, keyword = 'morty', page = 0}={}){
     let apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=g&lang=en`;
 
     return fetch(apiURL).then(res => res.json()).then(response => {
