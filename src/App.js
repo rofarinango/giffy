fafaa093e1fc43c4613dd0a6d840de642a48ca05
Rodrigo Farinango <rodrigofarii@gmail.com>
2023-03-22 17:58:51 -0600

    add error handle 404

--- src/App.js
+++ src/App.js
@@ -25,6 +25,10 @@ function App() {
               component={Detail}
               path='/gif/:id'
             />
+            <Route
+              component={() => <h1>404 ERROR :(</h1>}
+              path='/404'
+            />
           </GifsContextProvider>
       </section>
     </div>
